2038a3f184865dbfa8395966733058fee8a6e782
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 16:19:39 +0300

    seventh try

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,12 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        double i = 0;
         if (power >= 0) {
-            while (i <= power) {
-                double result = Math.pow(2, i);
+            int e;
+            int result;
+            for (int i = 0; i < power; i++) {
+                result = 1;
+                e = i;
+                while (e > 0) {
+                    result *= 2;
+                    e--;
+                }
                 System.out.println(result);
-                i++;
             }
         } else {
             System.out.println("too much power");
