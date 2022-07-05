5ad009d4f0499079b94246a495aac0f6bd04f8ef
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 14:14:56 +0300

    first try

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,9 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-        int result = 1;
+
         int counter = 0;
         while (counter <= printToInclusive) {
+            int result = 1;
             result = counter * result;
             System.out.println(result);
             counter++;
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int count = 0;
+        while (count <= printToInclusive) {
+            if (count != 2 && count % 2 == 0 || count % 3 == 0 || count % 4 == 0 || count % 5 == 0) {
+                count++;
+            } else {
+                System.out.println(count);
+                count++;
+            }
+        }
     }
 }
