4aa393b82b205f1943f73b30850674f8e71a67eb
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 13:20:34 +0300

    first try

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int result = 1;
+        int counter = 0;
+        while (counter <= printToInclusive) {
+            result = counter * result;
+            System.out.println(result);
+            counter++;
 
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int mult = 0;
+        while (mult <= multiplyByAndToInclusive) {
+            System.out.println(mult * multiplyByAndToInclusive);
+            mult++;
+        }
     }
 }
