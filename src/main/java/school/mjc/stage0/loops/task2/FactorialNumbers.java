cdae35b941689e9d3db48eb80e1b96e824f38a48
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 14:23:22 +0300

    second try

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
         int counter = 0;
         while (counter <= printToInclusive) {
             int result = 1;
